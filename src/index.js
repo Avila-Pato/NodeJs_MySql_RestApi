42fc177378967fa02088ce76aa61f24baade163d
Avila-Pato <p.avilaf1998@gmail.com>
2024-09-13 18:19:09 -0300

    Ruta no encontrada

--- src/index.js
+++ src/index.js
@@ -11,4 +11,13 @@ app.use(express.json())
 app.use(indexRoutes)
 app.use('/api', employeesRoutes)
 
+
+// Ruta no encontrada
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'endpoint not found',
+  })
+})
+
 app.listen(3000)
+console.log('Server running on port 3000')
